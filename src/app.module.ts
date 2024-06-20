b4deb0c95e9332f1cf2ccf29ca007593ac4d2011
DevNickjr <nicholasduadei14@gmail.com>
2024-06-21 00:11:32 +0100

    setup modules:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { SchoolsModule } from './schools/schools.module';
+import { ClassModule } from './class/class.module';
+import { TeachersModule } from './teachers/teachers.module';
+import { StudentsModule } from './students/students.module';
+import { SubjectsModule } from './subjects/subjects.module';
+
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, SchoolsModule, ClassModule, TeachersModule, StudentsModule, SubjectsModule],
   controllers: [AppController],
   providers: [AppService],
 })
