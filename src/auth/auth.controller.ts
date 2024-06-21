ce993a275a73a83c254d6690afbc95f726b9b2e1
DevNickjr <nicholasduadei14@gmail.com>
2024-06-21 19:54:14 +0100

    setup auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,42 +1,23 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post, Body, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Post('register')
+  register(@Body() createAuthDto: CreateUserDto) {
+    return this.authService.register(createAuthDto);
+  }
+
   @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  login(
+    @Body() createAuthDto: CreateAuthDto,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    return this.authService.login(createAuthDto, response);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { UnauthorizedException } from '@nestjs/common';
+import bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
+
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    private readonly usersService: UsersService, 
+    private jwtService: JwtService
+  ) {}
+
+  async register(createUserDto: CreateUserDto) {
+    const { password: userPassword, ...other } = createUserDto;
+    const password = await this.hashPassword(userPassword)
+    return await this.usersService.create({ ...other, password });
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async login(createAuthDto: CreateAuthDto, response: Response) {
+    const user = await this.usersService.findOne(createAuthDto.email);
+
+    if (!user) {
+      throw new BadRequestException('User does not exist')
+    }
+    const { password, ...result } = user;
+
+    await this.comparePassword(password, createAuthDto.password)
+
+    const payload = { sub: user._id, role: user.role, email: user.email };
+
+    const expires = new Date();
+    expires.setSeconds(expires.getSeconds() + 360000);
+
+    const token = this.jwtService.sign(payload);
+
+    return response.cookie('Authentication', token, {
+      httpOnly: true,
+      expires,
+      path: '/',
+    });
+
+    // return {
+    //   access_token: await this.jwtService.signAsync(payload),
+    // };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
+  async hashPassword(password: string): Promise<string> {
+    try {
+      return await bcrypt.hash(password, 10);
+    } catch (e) {
+      console.log(e);
+      throw new UnauthorizedException("You're not authorized");
+    }
   }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+ 
+  async comparePassword(password1: string, password2): Promise<string> {
+    try {
+      return await bcrypt.compare(password1, password2);
+    } catch (e) {
+      console.log(e);
+      throw new UnauthorizedException("You're not authorized");
+    }
   }
 }
