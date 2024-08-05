664d3da0845b7108185ec466c2de477e7fa36830
DevNickjr <nicholasduadei14@gmail.com>
2024-08-05 21:37:19 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:3000',
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
   });
-  await app.listen(3000);
+  await app.listen(3200);
 }
 bootstrap();
